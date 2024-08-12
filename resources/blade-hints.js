f8936ec9e8b85c284fdfdc503e1eb7462d96f88d
gwleuverink <willem@leuver.ink>
2024-08-13 01:51:39 +0200

    improve positioning

--- resources/blade-hints.js
+++ resources/blade-hints.js
@@ -1,10 +1,13 @@
 import "./blade-hints.css";
 
-import tippy from "tippy.js";
+import tippy, { inlinePositioning } from "tippy.js";
 import "tippy.js/dist/tippy.css";
 
 document.addEventListener("DOMContentLoaded", function () {
   tippy(".blade-hints", {
+    interactive: true,
+    inlinePositioning: true,
+    plugins: [inlinePositioning],
     content: (reference) => reference.dataset.bladeHintsLabel,
   });
 });
