14427debc5e11cb56e16bad321a0263eb9ae42b9
gwleuverink <willem@leuver.ink>
2024-08-12 23:02:10 +0200

    wip - setup testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,28 @@
 
 namespace Tests;
 
-use PHPUnit\Framework\TestCase as BaseTestCase;
+use Mockery as m;
+use Illuminate\Filesystem\Filesystem;
+use Leuverink\Glimpse\View\BladeCompiler;
+use Orchestra\Testbench\Concerns\WithWorkbench;
+use Illuminate\View\Compilers\CompilerInterface;
+use Orchestra\Testbench\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use WithWorkbench;
+
+    protected CompilerInterface $compiler;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->compiler = new BladeCompiler($this->getFiles(), __DIR__);
+    }
+
+    protected function getFiles()
+    {
+        return m::mock(Filesystem::class);
+    }
 }
